fb1f1dc487857a6b9cc1477eb86b5b5953983c7c
beezzlot <57652313+beezzlot@users.noreply.github.com>
2023-09-02 14:17:25 +0500

    Update vkcs_provider.tf

--- vkcs_provider.tf
+++ vkcs_provider.tf
@@ -9,10 +9,10 @@ terraform {
 
 provider "vkcs" {
     # Your user account.
-    username = "inimesepoeg@vk.com"
+    username = ""
 
     # The password of the account
-    password = "608976Bee$$"
+    password = ""
 
     # The tenant token can be taken from the project Settings tab - > API keys.
     # Project ID will be our token.
